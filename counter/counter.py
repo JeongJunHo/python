91feaa887a2516486de7da51e005f83ed8604ee2
JeongJunHo <jjh49470826@gmail.com>
2020-03-25 15:13:03 +0900

    new: Python counter
    python counter 예제 추가

---
+++ counter/counter.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def countLetters(word):
+    counter = {}
+    for letter in word:
+        if letter not in counter:
+            counter[letter] = 0
+        counter[letter] += 1
+    return counter
+
+print(countLetters('hello world'))
+# out = {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
+print(Counter('hello world'))
+# out = {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
+print(Counter('hello world').most_common())
+# out = [('l', 3), ('o', 2), ('h', 1), ('e', 1), (' ', 1), ('w', 1), ('r', 1), ('d', 1)]
+print(Counter('hello world').most_common(2))
+# out = [('l', 3), ('o', 2)]
---
+++ counter/readme.md
@@ -0,0 +1,12 @@
+# python Counter
+
+python collections 모듈의 Counter 사용 예제
+
+## how to run
+
+```
+git clone https://github.com/JeongJunHo/python.git
+cd counter
+counter.py
+```
+
